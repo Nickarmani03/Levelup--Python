71a6ca05cc0e51c59ca4414f36997b572c6f0b46
Nickarmani03 <nickarmani03@gmail.com>
2021-08-26 09:23:57 -0500

    added reporting fuinctionality for events

--- levelupreports/urls.py
+++ levelupreports/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from .views import usergame_list
+from .views import userevent_list
 
 urlpatterns = [
     path('reports/usergames', usergame_list),
+]
+
+urlpatterns = [
+    path('reports/userevents', userevent_list),
 ]
--- levelupreports/views/users/eventsbyuser.py
+++ levelupreports/views/users/eventsbyuser.py
@@ -1,11 +1,11 @@
 """Module for generating games by user report"""
 import sqlite3
 from django.shortcuts import render
-from levelupapi.models import Game
+from levelupapi.models import Event
 from levelupreports.views import Connection
 
 
-def usergame_list(request):
+def userevent_list(request):
     """Function to build an HTML report of games by user"""
     if request.method == 'GET':
         # Connect to project database
@@ -16,20 +16,21 @@ def usergame_list(request):
             # Query for all games, with related user info.
             db_cursor.execute("""
                 SELECT
-                    g.id,
-                    g.name,
-                    g.maker,
-                    g.game_type_id,
-                    g.number_of_players,
-                    g.description,
+                    g.id AS user_id,
+                    e.id,
+                    e.date,
+                    e.time,                
+                    e.description,
+                    e.title,
+                    gm.name,
                     u.id user_id,
                     u.first_name || ' ' || u.last_name AS full_name
-                FROM
-                    levelupapi_game g
-                JOIN
-                    levelupapi_gamer gr ON g.gamer_id = gr.id
-                JOIN
-                    auth_user u ON gr.user_id = u.id
+                FROM levelupapi_event e
+                JOIN levelupapi_eventgamer eg
+                    ON e.id = eg.event_id
+                JOIN levelupapi_gamer g ON eg.gamer_id = g.id
+                JOIN auth_user u ON u.id = g.user_id
+                JOIN levelupapi_game gm ON e.game_id = gm.id;
             """)
 
             dataset = db_cursor.fetchall()
@@ -45,7 +46,7 @@ def usergame_list(request):
             #             {
             #                 "id": 1,
             #                 "name": "Foo",
-            #                 "maker": "Bar Games",
+            #                 "date": "Bar Games",
             #                 "description":"awesome",
             #                 "number_of_players": 4,
             #                 "game_type_id": 2
@@ -54,40 +55,40 @@ def usergame_list(request):
             #     }
             # }
 
-            games_by_user = {}
+            events_by_user = {}
 
             for row in dataset:
                 # Crete a Game instance and set its properties
-                game = Game()
-                game.name = row["name"]
-                game.maker = row["maker"]
-                game.description = row["description"]
-                game.number_of_players = row["number_of_players"]
-                game.game_type_id = row["game_type_id"]
+                event = Event()
+                event.title = row["name"]
+                event.date = row["date"]
+                event.time = row["time"]
+                event.description = row["description"]                
+                
 
                 # Store the user's id
                 uid = row["user_id"]
 
                 # If the user's id is already a key in the dictionary...
-                if uid in games_by_user:
+                if uid in events_by_user:
 
                     # Add the current game to the `games` list for it
-                    games_by_user[uid]['games'].append(game)
+                    events_by_user[uid]['events'].append(event)
 
                 else:
                     # Otherwise, create the key and dictionary value
-                    games_by_user[uid] = {}
-                    games_by_user[uid]["id"] = uid
-                    games_by_user[uid]["full_name"] = row["full_name"]
-                    games_by_user[uid]["games"] = [game]
+                    events_by_user[uid] = {}
+                    events_by_user[uid]["id"] = uid
+                    events_by_user[uid]["full_name"] = row["full_name"]
+                    events_by_user[uid]["events"] = [event]
 
         # Get only the values from the dictionary and create a list from them
-        list_of_users_with_games = games_by_user.values()
+        list_of_events_with_users = events_by_user.values()
 
         # Specify the Django template and provide data context
-        template = 'users/list_with_games.html'
+        template = 'users/list_with_events.html'
         context = {
-            'usergame_list': list_of_users_with_games
+            'userevent_list': list_of_events_with_users
         }
 
         return render(request, template, context)
